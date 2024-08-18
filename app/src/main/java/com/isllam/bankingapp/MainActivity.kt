f03220f0e0b457f177cddefb9fc0762ce6f73c4e
islam <isllamessam@gmail.com>
2024-08-18 20:51:38 +0300

    UI FINISHED

--- app/src/main/java/com/isllam/bankingapp/MainActivity.kt
+++ app/src/main/java/com/isllam/bankingapp/MainActivity.kt
@@ -66,7 +66,7 @@ fun HomeScreen() {
             CardsSection()
             Spacer(modifier = Modifier.height(16.dp))
             FinanceSection()
-            // CurrenciesSection()
+            CurrenciesSection()
         }
 
     }
